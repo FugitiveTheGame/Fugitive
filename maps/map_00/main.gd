6f8dba839f7060c7e87c0874265ad19ffbdd395b
Adam Brown <adamwbrown@gmail.com>
2019-10-18 15:59:37 -0500

    Multiple hiders supported

--- maps/map_00/main.gd
+++ maps/map_00/main.gd
@@ -3,35 +3,49 @@ extends Node2D
 onready var detectionLabel := get_node("TestDetectionLabel")
 onready var seeker := get_node("seeker")
 onready var seeker_ray_caster := get_node("seeker/RayCast2D")
-onready var hider_00 := get_node("hider_test")
-onready var hider_00_colider := hider_00.get_node("Body")
+
 var cone_width = deg2rad(45.0)
 var max_detect_distance := 300.0
 
 func _ready():
 	detectionLabel.hide()
-	hider_00.modulate.a = 0.0
 
-func _process(delta):
-	var look_vec = seeker.to_local(hider_00.rect_global_position)
+func _physics_process(delta):
+	var anySeen := false
+	
+	var hiders = get_tree().get_nodes_in_group("hiders")
+	for hider in hiders:
+		if(process_hider(hider)):
+			anySeen = true
+	
+	if(anySeen):
+		detectionLabel.show()
+	else:
+		detectionLabel.hide()
+
+func process_hider(hider) -> bool:
+	var isSeen = false
+	
+	var look_vec = seeker.to_local(hider.rect_global_position)
 	seeker_ray_caster.cast_to = look_vec
 	
 	var look_angle = atan2(look_vec.y, look_vec.x)
 	seeker_ray_caster.force_raycast_update()
 	if(seeker_ray_caster.is_colliding()):
-		var colliderSeen = seeker_ray_caster.get_collider()
-		var distance = seeker.global_position.distance_to(hider_00.rect_global_position)
+		var bodySeen = seeker_ray_caster.get_collider()
+		var distance = seeker.global_position.distance_to(hider.rect_global_position)
+		var hiderBody = hider.get_node("Body")
 		
-		if(colliderSeen == hider_00_colider):
+		if(bodySeen == hiderBody):
 			var percent_visible = 1.0 - clamp(abs(look_angle / cone_width), 0.0, 1.0)
-			hider_00.modulate.a = percent_visible
+			hider.modulate.a = percent_visible
+			#print("visible: %f" % percent_visible)
 			
 			if(look_angle < cone_width
 					and look_angle  > -cone_width
 					and distance <= max_detect_distance):
-				# Should set the alpha of hider based on how far the angle is from zero
-				detectionLabel.show()
-			else:
-				detectionLabel.hide()
+				isSeen = true
 		else:
-			hider_00.modulate.a = 0.0
+			hider.modulate.a = 0.0
+	
+	return isSeen
