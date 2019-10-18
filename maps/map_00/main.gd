1a1ae5ae53c172adf2cad1e2945a62c41f6c1bd7
Adam Brown <adamwbrown@gmail.com>
2019-10-18 13:46:27 -0500

    Nicer hide/show
    Hider now fades in and out depending on how centered he is in the light cone.
    
    The current code only works for 1 hider, must fix.

--- maps/map_00/main.gd
+++ maps/map_00/main.gd
@@ -10,22 +10,28 @@ var max_detect_distance := 300.0
 
 func _ready():
 	detectionLabel.hide()
+	hider_00.modulate.a = 0.0
 
 func _process(delta):
 	var look_vec = seeker.to_local(hider_00.rect_global_position)
 	seeker_ray_caster.cast_to = look_vec
 	
 	var look_angle = atan2(look_vec.y, look_vec.x)
+	seeker_ray_caster.force_raycast_update()
 	if(seeker_ray_caster.is_colliding()):
 		var colliderSeen = seeker_ray_caster.get_collider()
 		var distance = seeker.global_position.distance_to(hider_00.rect_global_position)
 		
-		if(colliderSeen == hider_00_colider
-			and look_angle < cone_width
-			and look_angle  > -cone_width
-			and distance <= max_detect_distance):
-				hider_00.show()
+		if(colliderSeen == hider_00_colider):
+			var percent_visible = 1.0 - clamp(abs(look_angle / cone_width), 0.0, 1.0)
+			hider_00.modulate.a = percent_visible
+			
+			if(look_angle < cone_width
+					and look_angle  > -cone_width
+					and distance <= max_detect_distance):
+				# Should set the alpha of hider based on how far the angle is from zero
 				detectionLabel.show()
+			else:
+				detectionLabel.hide()
 		else:
-			hider_00.hide()
-			detectionLabel.hide()
+			hider_00.modulate.a = 0.0
