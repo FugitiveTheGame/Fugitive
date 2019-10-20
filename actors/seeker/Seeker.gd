126790989f670d073a2635d9ec1a5fb20dce45e2
Adam Brown <adamwbrown@gmail.com>
2019-10-20 16:52:45 -0600

    Seeker movement is working!

--- actors/seeker/Seeker.gd
+++ actors/seeker/Seeker.gd
@@ -7,7 +7,16 @@ export (float) var rotation_speed = 1.5
 var velocity = Vector2()
 var rotation_dir = 0
 
-func get_input():
+puppet func setVelocity(vel):
+  velocity = vel
+
+puppet func setRotation(rot):
+  rotation = rot
+
+func get_input(delta):
+	if not is_network_master():
+		return
+	
 	rotation_dir = 0
 	velocity = Vector2()
 	if Input.is_action_pressed('ui_right'):
@@ -18,11 +27,12 @@ func get_input():
 		velocity = Vector2(-speed, 0).rotated(rotation)
 	if Input.is_action_pressed('ui_up'):
 		velocity = Vector2(speed, 0).rotated(rotation)
+	rpc_unreliable("setVelocity",velocity)
+	
+	rotation += rotation_dir * rotation_speed * delta
+	rpc_unreliable("setRotation",rotation)
 
 func _physics_process(delta):
-	if not is_network_master():
-		return
+	get_input(delta)
 	
-	get_input()
-	rotation += rotation_dir * rotation_speed * delta
 	velocity = move_and_slide(velocity)
