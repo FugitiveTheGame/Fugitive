8b8f3804e5c4ef00bc017de704f448116ceb72f3
Adam Brown <adamwbrown@gmail.com>
2019-10-21 16:39:49 -0600

    Map_00_dev is now working!

--- maps/map_00/Map_00_dev.gd
+++ maps/map_00/Map_00_dev.gd
@@ -2,8 +2,14 @@ extends "res://maps/map_00/Map_00.gd"
 
 func _ready():
 	$players/Seeker00.set_current_player()
-
-func pre_configure_game():
+	$players/Seeker00.set_network_master(1)
+	
+	$players/Hider00.set_network_master(2)
+	$players/Hider01.set_network_master(2)
+	
 	var peer = NetworkedMultiplayerENet.new()
 	assert(peer.create_server(Network.DEFAULT_PORT, Network.MAX_PLAYERS) == OK)
 	get_tree().set_network_peer(peer)
+
+func pre_configure_game():
+	pass
