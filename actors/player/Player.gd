e97eca90fd84f429529c83d8c9a62251c9a5562e
Adam Brown <adamwbrown@gmail.com>
2019-10-22 08:59:22 -0700

    Implemented player sprinting

--- actors/player/Player.gd
+++ actors/player/Player.gd
@@ -4,17 +4,27 @@ class_name Player
 export (int) var speed = 200
 export (float) var rotation_speed = 1.5
 export (String) var playerName: String
+export (float) var max_stamina = 100.0
+export (float) var stamina_depletion_rate = 100.0
+export (float) var stamina_regen_rate = 25.0
+export (float) var sprint_speed = 400.0
 
-onready var camera := $Camera
-onready var footStepAudio := $FootStepAudio
-onready var playerNameLabel := $PlayerNameLabel
+onready var camera := $Camera as Camera2D
+onready var footStepAudio := $FootStepAudio as AudioStreamPlayer2D
+onready var playerNameLabel := $PlayerNameLabel as Label
+onready var staminaBar := $StaminaBar as ProgressBar
 
 var velocity := Vector2()
 var rotation_dir := 0
 
+var stamina: float
+
 var frozen := false
+var frozenColor := Color.blue
 
 func _ready():
+	stamina = max_stamina
+	staminaBar.max_value = max_stamina
 	playerNameLabel.text = playerName
 
 puppet func setNetworkPosition(pos: Vector2):
@@ -25,6 +35,9 @@ puppet func setNetworkVelocity(vel: Vector2):
 	
 puppet func setNetworkRotation(rot: float):
 	self.rotation = rot
+
+puppet func setNetworkStamina(stam: float):
+	self.stamina = stam
 	
 func unfreeze():
 	self.frozen = false
@@ -33,17 +46,24 @@ func unfreeze():
 func freeze():
 	self.frozen = true
 	self.velocity = Vector2(0, 0)
-	self.modulate.b = 1.0
-	self.modulate.r = 0.0
-	self.modulate.g = 0.0
-	self.modulate.a = 1.0
-	
-func get_input(delta):
+	self.modulate = frozenColor
+
+# warning-ignore:unused_argument
+func _process(delta: float):
+	staminaBar.value = stamina
+
+func get_input(delta: float):
 	if not is_network_master():
 		return
 	
 	if (self.frozen):
 		return
+	
+	var curSpeed: float
+	if is_sprinting():
+		curSpeed = sprint_speed
+	else:
+		curSpeed = speed
 	
 	rotation_dir = 0
 	self.velocity = Vector2()
@@ -52,21 +72,33 @@ func get_input(delta):
 	if Input.is_action_pressed('ui_left'):
 		self.rotation_dir -= 1
 	if Input.is_action_pressed('ui_down'):
-		self.velocity = Vector2(-self.speed, 0).rotated(self.rotation)
+		self.velocity = Vector2(-curSpeed, 0).rotated(self.rotation)
 	if Input.is_action_pressed('ui_up'):
-		self.velocity = Vector2(self.speed, 0).rotated(self.rotation)
+		self.velocity = Vector2(curSpeed, 0).rotated(self.rotation)
 	
 	rotation += self.rotation_dir * self.rotation_speed * delta
 
-func _physics_process(delta):
+func is_sprinting():
+	return Input.is_action_pressed('sprint') and stamina > 0.0
+
+func process_stamina(delta: float):
+	if is_sprinting():
+		stamina -= (stamina_depletion_rate * delta)
+	elif not Input.is_action_pressed('sprint') and not is_moving():
+		stamina += (stamina_regen_rate * delta)
+	stamina = clamp(stamina, 0.0, max_stamina)
+
+func _physics_process(delta: float):
 	get_input(delta)
 	
 	if is_network_master():
 		self.velocity = move_and_slide(self.velocity)
+		process_stamina(delta)
 		
 		rpc_unreliable("setNetworkPosition", self.position)
 		rpc_unreliable("setNetworkVelocity", self.velocity)
 		rpc_unreliable("setNetworkRotation", self.rotation)
+		rpc_unreliable("setNetworkStamina", self.stamina)
 	
 	# Make movement noises if moving
 	if is_moving():
--- screens/lobby/ControlPlayerLabel.gd
+++ screens/lobby/ControlPlayerLabel.gd
@@ -1,13 +1,6 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var playerId := 0;
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func setPlayerId(value: int):
 	self.playerId = value
